70a33c3a5ad45187cf50cf2386cc6a4dac940237
veronica-ivanova <veronica.ivanova27@gmail.com>
2022-04-07 17:59:38 +0300

    Muiliple

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Veronika");
+        System.out.println("27/08/1993");
+
+    }
+}
