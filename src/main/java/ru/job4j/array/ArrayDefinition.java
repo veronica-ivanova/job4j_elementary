7bdb219c14cb4e3b135b66805828ef55160d2f4d
veronica-ivanova <veronica.ivanova27@gmail.com>
2022-04-16 18:51:48 +0300

    6.0.2. Заполнение массива.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("размер массива " + surnames.length);
         float[] prices = new float[40];
         System.out.println("размер массива " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Ivanov";
+        names[1] = "Ivan Petrov";
+        names[2] = "Mary Ivanova";
+        names[3] = "Alex Petrov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
