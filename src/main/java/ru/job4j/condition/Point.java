9c60ec24b3263602bcd414e3b5ce306f1790ca0d
veronica-ivanova <veronica.ivanova27@gmail.com>
2022-05-01 21:01:43 +0300

    2.1. Рефакторинг - Расстояние между точками.

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,18 +1,40 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        return rsl;
+//    public static double distance(int x1, int y1, int x2, int y2) {
+//        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+//        return rsl;
+//    }
+//
+//    public static void main(String[] args) {
+//        double result = Point.distance(0, 0, 2, 0);
+//        System.out.println("result (0, 0) to (2, 0) " + result);
+//        result = Point.distance(5, 6, 4, 8);
+//        System.out.println("result (5, 6) to (4, 8) " + result);
+//        result = Point.distance(-150, 178, 100, 190);
+//        System.out.println("result (-150, 178) to (100, 190) " + result);
+//    }
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(5, 6, 4, 8);
-        System.out.println("result (5, 6) to (4, 8) " + result);
-        result = Point.distance(-150, 178, 100, 190);
-        System.out.println("result (-150, 178) to (100, 190) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -5,36 +5,46 @@ import org.junit.Test;
 
 public class PointTest {
 
-    @Test
-    public void when00to20then2() {
-        double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when56to48then2dot23() {
-        double expected = 2.23;
-        int x1 = 5;
-        int y1 = 6;
-        int x2 = 4;
-        int y2 = 8;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
+//    @Test
+//    public void when00to20then2() {
+//        double expected = 2;
+//        int x1 = 0;
+//        int y1 = 0;
+//        int x2 = 2;
+//        int y2 = 0;
+//        double out = Point.distance(x1, y1, x2, y2);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
+//
+//    @Test
+//    public void when56to48then2dot23() {
+//        double expected = 2.23;
+//        int x1 = 5;
+//        int y1 = 6;
+//        int x2 = 4;
+//        int y2 = 8;
+//        double out = Point.distance(x1, y1, x2, y2);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
+//
+//    @Test
+//    public void when150178to100190then250dot29() {
+//        double expected = 250.29;
+//        int x1 = -150;
+//        int y1 = 178;
+//        int x2 = 100;
+//        int y2 = 190;
+//        double out = Point.distance(x1, y1, x2, y2);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
 
     @Test
     public void when150178to100190then250dot29() {
         double expected = 250.29;
-        int x1 = -150;
-        int y1 = 178;
-        int x2 = 100;
-        int y2 = 190;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(-150, 178);
+        Point b = new Point(100, 190);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
+
     }
 }
